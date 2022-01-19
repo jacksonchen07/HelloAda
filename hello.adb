d30e6efab28cbbc0bc80e4542f64aa1bab06ebf6
BKambic <98051852+BKambic@users.noreply.github.com>
2022-01-19 13:03:12 -0600

    Update hello.adb
    Added a Goodbye and had it alternate.

--- hello.adb
+++ hello.adb
@@ -3,5 +3,16 @@ with Ada.Text_IO;
 procedure Hello is
     use Ada.Text_IO;
 begin
-    Put_Line ("Hello Ada!");
+
+    for I in 1..6 loop
+
+        if 2 mod I = 0 then -- the first 3rd and 5th loop says hello and the 2nd 4ht and 6th loop says goodbye
+            Put_line ("Goodbye Ada!");
+    
+         else
+             Put_Line ("Hello Ada!");
+         end if;
+    
+    end loop;    
+    
 end Hello;
